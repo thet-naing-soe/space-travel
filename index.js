d485cb2286db137531e82a07c541f91913e25213
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-02-09 12:53:52 +0630

    adding animations and changing the button icon

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const navToggle = document.querySelector(".mobile-nav-toggle");
 
 navToggle.addEventListener("click", () => {
   const visiblity = nav.getAttribute("data-visible");
-  if(visiblity === "false") {
-    nav.setAttribute("data-visible", true)
+  if (visiblity === "false") {
+    nav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
   } else {
-    nav.setAttribute("data-visible", false)
+    nav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
   }
 });
